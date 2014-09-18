fe5a4da3b6760dd0410e58f9878de8cc6fe653da
Ricardo Bahillo <Ricardo.Bahillo@odigeo.com>
2014-09-19 00:12:39 +0200

    Added orientation capability and button

--- Hello Time/IAViewController.m
+++ Hello Time/IAViewController.m
@@ -19,6 +19,19 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self checkTime:self];
+    NSLog(@"viewDidLoad: called");
+}
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidAppear: called");
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"viewWillAppear: called");
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,13 +52,41 @@
     
     if ([self.modeButton.titleLabel.text isEqualToString: @"Night"]){
         self.view.backgroundColor = [UIColor blackColor];
-        self.timeLabel.backgroundColor = [UIColor whiteColor];
+        self.timeLabel.textColor = [UIColor whiteColor];
         [self.modeButton setTitle:@"Day" forState:UIControlStateNormal];
     }
     else{
         self.view.backgroundColor = [UIColor whiteColor];
-        self.timeLabel.backgroundColor = [UIColor blackColor];
+        self.timeLabel.textColor = [UIColor blackColor];
         [self.modeButton setTitle:@"Night" forState:UIControlStateNormal];
     }
+}
+
+- (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+    CGRect timeFrame = self.timeLabel.frame;
+    float viewHeight = self.view.frame.size.height;
+    float viewWidth = self.view.frame.size.width;
+    float fontSize = 30.0;
+    BOOL hideButton = YES;
+    
+    if(UIInterfaceOrientationIsLandscape(self.interfaceOrientation)){
+        fontSize=40.0;
+        timeFrame.origin.y=(viewWidth/2) - timeFrame.size.height;
+        timeFrame.size.width=viewWidth;
+    }
+    else{
+        hideButton=NO;
+        timeFrame.origin.y=(viewHeight/2) - timeFrame.size.height;
+        timeFrame.size.width=viewWidth;
+    }
+    
+    [self.modeButton setHidden:hideButton];
+    [self.timeLabel setFont:[UIFont boldSystemFontOfSize:fontSize]];
+    [self.timeLabel setFrame:timeFrame];
+    
+}
+
+- (NSUInteger) supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskLandscape | UIInterfaceOrientationMaskPortrait;
 }
 @end
